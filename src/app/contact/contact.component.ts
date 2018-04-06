498830c9e198ad5dfa0b5f4cf4291746d79f0302
Steen G. Sargent <ssargent@crossroadsnet.com>
2018-04-06 14:20:22 -0600

    contact form with sweet alert

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact',
@@ -10,6 +11,18 @@ export class ContactComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submit(f) {
+    Swal({
+      title: 'Awesome!',
+      text: 'Thanks for your comment!',
+      type: 'success',
+    }).then(() => {
+      window.location.href = "http://localhost:4200"
+      console.log(window.location.href)
+    })
+    console.log(f.value)
   }
 
 }
